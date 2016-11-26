9e0bd2291ea59a1bd7dfc990066588dcc586e5f0
Yuretzz <yuvede@gmail.com>
2016-11-26 10:42:56 +0300

    adapted raw edid for py3

--- edid_parser.py
+++ edid_parser.py
@@ -4,10 +4,12 @@
   http://en.wikipedia.org/wiki/Extended_display_identification_data
 """
 import itertools
+
 try:
     zip_longest = itertools.zip_longest
 except AttributeError:
     zip_longest = itertools.izip_longest
+
 
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
@@ -16,16 +18,18 @@ def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
 
+
 # Constants lifted from EDID documentation.
 # a. Constants needed for verification
-MINIMAL_SIZE = 128 # EDID record must be not less than 128 Bytes
-VERSION = 0x01
-MAGIC = '\x00\xff\xff\xff\xff\xff\xff\x00'
+MINIMAL_SIZE = 128  # EDID record must be not less than 128 Bytes
+VERSION = 1
+# MAGIC = '\x00\xff\xff\xff\xff\xff\xff\x00' # was for Py 2
+MAGIC = bytearray([0, 255, 255, 255, 255, 255, 255, 0])  # for Py 3
 MAGIC_OFFSET = 0
-VERSION_OFFSET = 18         # 12h
-REVISION_OFFSET = 19        # 13h
-CHECKSUM_OFFSET = 127       # 7Fh
-PIXEL_CLOCK_OFFSET = 54     # 36h
+VERSION_OFFSET = 18  # 12h
+REVISION_OFFSET = 19  # 13h
+CHECKSUM_OFFSET = 127  # 7Fh
+PIXEL_CLOCK_OFFSET = 54  # 36h
 # b. Constants for monitor info
 MANUFACTURER_ID_OFFSET = 8
 MANUFACTURER_ID_BITS = 5
@@ -34,10 +38,10 @@ PRODUCT_SERIAL_OFFSET = 12  # 0Ch
 WEEK_OFFSET = 16
 YEAR_OFFSET = 17
 WIDTH_CM_OFFSET = 21
-HEIGHT_CM_OFFSET = 22    # often bullshit. will calculate based on aspect ratio
+HEIGHT_CM_OFFSET = 22  # often bullshit. will calculate based on aspect ratio
 FEATURES24_OFFSET = 24
-FORMER_PIXDIM_OFFSET = 35   # 23h
-TIMING_BLOCK_OFFSET = 38    # 26h
+FORMER_PIXDIM_OFFSET = 35  # 23h
+TIMING_BLOCK_OFFSET = 38  # 26h
 DESCR_1_OFFSET = 54
 
 HORIZONTAL_OFFSET = 56
@@ -48,35 +52,40 @@ VERTICAL_HIGH_OFFSET = 61
 
 def parse(blob):
     """EDID Parser"""
+
     def read_short(offset):
-        return ((ord(blob[offset]) << 8) | ord(blob[offset + 1]))
+        return (blob[offset] << 8) | blob[offset + 1]
+
     def read_litendian16(offset):
-        return ((ord(blob[offset + 1]) << 8) | ord(blob[offset]))
+        return (blob[offset + 1] << 8) | blob[offset]
+
     def read_litendian32(offset):
-        return ((ord(blob[offset + 3]) << 24) |
-                (ord(blob[offset + 2]) << 16) |
-                (ord(blob[offset + 1]) <<  8) |
-                 ord(blob[offset])
-               )
+        return ((blob[offset + 3] << 24) |
+                (blob[offset + 2] << 16) |
+                (blob[offset + 1] << 8) |
+                blob[offset]
+                )
+
     def read_bigendian24(offset):
-        return ((ord(blob[offset]) << 16) |
-                (ord(blob[offset + 1]) << 8) |
-                (ord(blob[offset + 2]))
-               )
+        return ((blob[offset] << 16) |
+                (blob[offset + 1] << 8) |
+                (blob[offset + 2])
+                )
+
     # a. VERIFICATION BLOCK
     # Check size, magic, and version
     if len(blob) < MINIMAL_SIZE:
         logging.warning("EDID parsing error: length too small.")
         return None
     if (blob[MAGIC_OFFSET:(MAGIC_OFFSET + len(MAGIC))] !=
-        MAGIC):
+            MAGIC):
         logging.warning("EDID parse error: incorrect header.")
         return None
-    if ord(blob[VERSION_OFFSET]) != VERSION:
+    if blob[VERSION_OFFSET] != VERSION:
         logging.warning("EDID parse error: unsupported EDID version.")
         return None
     # Verify checksum
-    if sum([ord(char) for char in blob[:CHECKSUM_OFFSET+1]]) % 0x100 != 0:
+    if sum([i for i in blob[:CHECKSUM_OFFSET + 1]]) % 0x100 != 0:
         logging.warning("EDID parse error: checksum error.")
         return None
     # Currently we don't support EDID not using pixel clock
@@ -96,84 +105,82 @@ def parse(blob):
         vendor_char = chr(vendor_char + ord('@'))
         vendor_name += vendor_char
     product_id = read_litendian16(PRODUCT_ID_OFFSET)
-    print vendor_name
-    print hex(product_id).split('x')[1].upper()
-    
+    print(vendor_name)
+    print(hex(product_id).split('x')[1].upper())
+
     serial = read_litendian32(PRODUCT_SERIAL_OFFSET)
-    if serial == 16843009: # if only 01 01 01 01h
-        serial = 0         # means no serial
-    print serial
+    if serial == 16843009:  # if only 01 01 01 01h
+        serial = 0  # means no serial
+    print(serial)
 
-    week = ord(blob[WEEK_OFFSET])
-    year = ord(blob[YEAR_OFFSET]) + 1990
-    print (week, year)
+    week = blob[WEEK_OFFSET]
+    year = blob[YEAR_OFFSET] + 1990
+    print((week, year))
 
-    widthcm = ord(blob[WIDTH_CM_OFFSET])
-    heightcm = ord(blob[HEIGHT_CM_OFFSET])
+    widthcm = blob[WIDTH_CM_OFFSET]
+    heightcm = blob[HEIGHT_CM_OFFSET]
 
-    prefferedResIndex = (ord(blob[FEATURES24_OFFSET]) & 0x2) >> 1
+    prefferedResIndex = (blob[FEATURES24_OFFSET] & 0x2) >> 1
 
     oldPixDims = read_bigendian24(FORMER_PIXDIM_OFFSET)
-    oldRes = {0 : (720, 400),
-              1 : (720, 400),
-              2 : (640, 480),
-              3 : (640, 480),
-              4 : (640, 480),
-              5 : (640, 480),
-              6 : (800, 600),
-              7 : (800, 600),
-              8 : (800, 600),
-              9 : (800, 600),
-              10 : (832, 624),
-              11 : (1024, 768),
-              12 : (1024, 768),
-              13 : (1024, 768),
-              14 : (1024, 768),
-              15 : (1280, 1024),
-              16 : (1152, 870)}
+    oldRes = {0: (720, 400),
+              1: (720, 400),
+              2: (640, 480),
+              3: (640, 480),
+              4: (640, 480),
+              5: (640, 480),
+              6: (800, 600),
+              7: (800, 600),
+              8: (800, 600),
+              9: (800, 600),
+              10: (832, 624),
+              11: (1024, 768),
+              12: (1024, 768),
+              13: (1024, 768),
+              14: (1024, 768),
+              15: (1280, 1024),
+              16: (1152, 870)}
     legacyRes = set()
-    print bin(oldPixDims)
+    print(bin(oldPixDims))
     for i in range(17):
         if (oldPixDims & (0x800000 >> i)) >> (23 - i):
             legacyRes.add(oldRes[i])
-                
-    print list(legacyRes)
 
-    widthmm =  (ord(blob[66]) |
-             ((ord(blob[68]) & 0xF0) << 4))
-    heightmm =  (ord(blob[67]) |
-             ((ord(blob[68]) & 0x0F) << 8))
-    
- 
-    
+    print(list(legacyRes))
+
+    widthmm = (blob[66] |
+               ((blob[68] & 0xF0) << 4))
+    heightmm = (blob[67] |
+                ((blob[68] & 0x0F) << 8))
+
     timingBlock = blob[TIMING_BLOCK_OFFSET:DESCR_1_OFFSET]
-    ratios = {0 : (16, 10),
-              1 : (4, 3),
-              2 : (5, 4),
-              3 : (16, 9)}
+    ratios = {0: (16, 10),
+              1: (4, 3),
+              2: (5, 4),
+              3: (16, 9)}
     for word in grouper(timingBlock, 2):
-        if (ord(word[0]), ord(word[0])) <> (1, 1):
-            Xresolution = (ord(word[0]) + 31) * 8
-            ind = (ord(word[1]) & 0xC0) >> 6
+        if (word[0], word[0]) != (1, 1):
+            Xresolution = (word[0] + 31) * 8
+            ind = (word[1] & 0xC0) >> 6
             ratio = ratios[ind]
             Yresolution = Xresolution / ratio[0] * ratio[1]
-            print 'puk', Xresolution, Yresolution, ratio
-    
-    width = (ord(blob[HORIZONTAL_OFFSET]) |
-             ((ord(blob[HORIZONTAL_HIGH_OFFSET]) >> 4) << 8))
-    height = (ord(blob[VERTICAL_OFFSET]) |
-              ((ord(blob[VERTICAL_HIGH_OFFSET]) >> 4) << 8))
-    
-    border = ord(blob[69])
-    print 'Border: ', border
-    
-    textDescType = ord(blob[93])
+            print('puk', Xresolution, Yresolution, ratio)
+
+    width = (blob[HORIZONTAL_OFFSET] |
+             ((blob[HORIZONTAL_HIGH_OFFSET] >> 4) << 8))
+    height = (blob[VERTICAL_OFFSET] |
+              ((blob[VERTICAL_HIGH_OFFSET] >> 4) << 8))
+
+    border = blob[69]
+    print('Border: ', border)
+
+    textDescType = blob[93]
     if textDescType in [255, 254, 252]:
         stri = str(blob[95:108])
         i = stri.find('\n')
-        print stri[:i]
-    
-    print widthcm, heightcm
-    print widthcm, widthcm*9/16
-    
-    return (widthmm, heightmm, width, height)
+        print(stri[:i])
+
+    print(widthcm, heightcm)
+    print(widthcm, widthcm * 9 / 16)
+
+    return widthmm, heightmm, width, height
--- moni_from_edid.py
+++ moni_from_edid.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import win32api # needs to download pywin32
+import win32api  # needs to download pywin32
 import ctypes
 from ctypes import sizeof, c_ulong, c_int64, c_void_p
 
@@ -13,10 +13,12 @@ from ctypes.wintypes import BYTE
 
 from edid_parser import parse
 
+
 # some details of the windows API differ between 32 and 64 bit systems..
 def is_64bit():
     """Returns true when running on a 64 bit system"""
     return sizeof(c_ulong) != sizeof(c_void_p)
+
 
 # ULONG_PTR is a an ordinary number, not a pointer and contrary to the name it
 # is either 32 or 64 bits, depending on the type of windows...
@@ -28,16 +30,19 @@ else:
     # 32 bits
     ULONG_PTR = c_ulong
 
+
 def ValidHandle(value, func, arguments):
     if value == 0:
         raise ctypes.WinError()
     return value
+
 
 NULL = 0
 HDEVINFO = ctypes.c_void_p
 PCTSTR = ctypes.c_char_p
 PDWORD = ctypes.POINTER(DWORD)
 REGSAM = DWORD
+
 
 class GUID(ctypes.Structure):
     _fields_ = [
@@ -56,6 +61,7 @@ class GUID(ctypes.Structure):
             ''.join(["%02x" % d for d in self.Data4[2:]]),
         )
 
+
 class SP_DEVINFO_DATA(ctypes.Structure):
     _fields_ = [
         ('cbSize', DWORD),
@@ -67,12 +73,14 @@ class SP_DEVINFO_DATA(ctypes.Structure):
     def __str__(self):
         return "ClassGuid:%s DevInst:%s" % (self.ClassGuid, self.DevInst)
 
+
 PSP_DEVINFO_DATA = ctypes.POINTER(SP_DEVINFO_DATA)
 
 setupapi = ctypes.windll.LoadLibrary("setupapi")
 
 SetupDiClassGuidsFromName = setupapi.SetupDiClassGuidsFromNameA
-SetupDiClassGuidsFromName.argtypes = [PCTSTR, ctypes.POINTER(GUID), DWORD, PDWORD]
+SetupDiClassGuidsFromName.argtypes = [PCTSTR, ctypes.POINTER(GUID), DWORD,
+                                      PDWORD]
 SetupDiClassGuidsFromName.restype = BOOL
 
 SetupDiGetClassDevs = setupapi.SetupDiGetClassDevsA
@@ -85,7 +93,8 @@ SetupDiEnumDeviceInfo.argtypes = [HDEVINFO, DWORD, PSP_DEVINFO_DATA]
 SetupDiEnumDeviceInfo.restype = BOOL
 
 SetupDiOpenDevRegKey = setupapi.SetupDiOpenDevRegKey
-SetupDiOpenDevRegKey.argtypes = [HDEVINFO, PSP_DEVINFO_DATA, DWORD, DWORD, DWORD, REGSAM]
+SetupDiOpenDevRegKey.argtypes = [HDEVINFO, PSP_DEVINFO_DATA, DWORD, DWORD,
+                                 DWORD, REGSAM]
 SetupDiOpenDevRegKey.restype = HKEY
 
 SetupDiDestroyDeviceInfoList = setupapi.SetupDiDestroyDeviceInfoList
@@ -103,11 +112,13 @@ DICS_FLAG_GLOBAL = 1
 DIREG_DEV = 0x00000001
 KEY_READ = 0x20019
 
-
 GUIDs = (GUID * 8)()  # so far only seen one used, so hope 8 are enough...
 guids_size = DWORD()
+# first argument is PCTSTR that should NOT be UNICODE. So, for Py3 compatibility
+# where all strings are Unicode we need to pass bytes object
+# bytes are specially designed to pass strict Ascii strings to other systems
 if not SetupDiClassGuidsFromName(
-        'Monitor',
+        'Monitor'.encode('ascii'),
         GUIDs,
         ctypes.sizeof(GUIDs),
         ctypes.byref(guids_size)):
@@ -118,10 +129,10 @@ for g in GUIDs:
 
 # device information set handle
 g_hdi = SetupDiGetClassDevs(
-        ctypes.byref(GUIDs[0]),
-        None,
-        NULL,
-        DIGCF_PRESENT)  # was DIGCF_PRESENT|DIGCF_DEVICEINTERFACE which misses CDC ports
+    ctypes.byref(GUIDs[0]),
+    None,
+    NULL,
+    DIGCF_PRESENT)  # was DIGCF_PRESENT|DIGCF_DEVICEINTERFACE which misses CDC ports
 
 print(g_hdi)
 
@@ -136,21 +147,20 @@ print(devinfo)
 
 # handle for an open registry key with EDID
 hkey = SetupDiOpenDevRegKey(
-        g_hdi,
-        ctypes.byref(devinfo),
-        DICS_FLAG_GLOBAL,
-        0,
-        DIREG_DEV,  # DIREG_DRV for SW info
-        KEY_READ)
+    g_hdi,
+    ctypes.byref(devinfo),
+    DICS_FLAG_GLOBAL,
+    0,
+    DIREG_DEV,  # DIREG_DRV for SW info
+    KEY_READ)
 
 print('hkey: ', hkey)
 
 # ZDESS !!!
 huj = win32api.RegEnumValue(hkey, 0)
-    
+
 RegCloseKey(hkey)
 SetupDiDestroyDeviceInfoList(g_hdi)
 
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-print parse(huj[1])
+print(parse(bytearray(huj[1])))
